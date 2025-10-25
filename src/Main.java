8f0e623f3a567502c2d4b0d7e7df61ca7835dad6
s.shlyandina <svk504@gmail.com>
2025-10-25 15:54:16 +0400

    Project5_scanner

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,22 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //создаем объект Car
-        Car car = new Car("Nissan", 2025);
-
-        //создать несколько экземпляров класса Car
-        Car car1 = new Car("BMV", 2016);
-        Car car2 = new Car("Lada Vesta", 2020);
-        Car car3 = new Car("Opel", 2017);
-        //переопределить переменные
-        car2.model = "Lada Priora";
-        car2.year = 2021;
-        //вывести результат метода BeBe
-
-        car2.Bebe();
+        Scanner scanner = new Scanner(System.in);
+        System.out.println ("Введите сумму вклада: ");
+        float initialSum = scanner.nextFloat();
+        System.out.println("Введите срок вклада в месяцах: ");
+        int number = scanner.nextInt();
+        float totalSum = initialSum;
+        final float PROCENT = 0.07f;
+        for (int a =0; a < number; a++) {
+            totalSum = totalSum * (1 + PROCENT);
+        }
+        System.out.println("После " + number + " месяцев сумма вклада составит: " + totalSum + " руб.");
+        scanner.close();
     }
 }
+
