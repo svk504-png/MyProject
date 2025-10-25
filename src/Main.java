53bd76869a8aaf68bd57a61f1f8334733aa410ee
s.shlyandina <svk504@gmail.com>
2025-10-25 16:04:02 +0400

    Project5_scanner_while

--- src/Main.java
+++ src/Main.java
@@ -6,14 +6,18 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println ("Введите сумму вклада: ");
+        System.out.println("Введите сумму вклада: ");
         float initialSum = scanner.nextFloat();
+
         System.out.println("Введите срок вклада в месяцах: ");
         int number = scanner.nextInt();
         float totalSum = initialSum;
+
         final float PROCENT = 0.07f;
-        for (int a =0; a < number; a++) {
+        int currentNumber = 0;
+        while (currentNumber < number) {
             totalSum = totalSum * (1 + PROCENT);
+            currentNumber++;
         }
         System.out.println("После " + number + " месяцев сумма вклада составит: " + totalSum + " руб.");
         scanner.close();
