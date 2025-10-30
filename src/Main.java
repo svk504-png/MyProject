e41f51ecf23917ae7657264b294cb4966b3f58c7
s.shlyandina <svk504@gmail.com>
2025-10-30 09:29:44 +0400

    Project7_2_Shape

--- src/Main.java
+++ src/Main.java
@@ -1,57 +1,40 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 
+import java.awt.*;
+import java.awt.geom.AffineTransform;
+import java.awt.geom.PathIterator;
+import java.awt.geom.Point2D;
+import java.awt.geom.Rectangle2D;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
 
-        //Создайте класс Employee с полями name и salary. Добавьте метод work(), который выводит сообщение о работе сотрудника.
-
-        class Employee {
-            protected String name;
-            protected int salary;
-
-            public Employee(String name, int salary) {
-                this.name = name;
-                this.salary = salary;
-            }
-
-            public void work() {
-                System.out.println(name + " работает аналитиком c окладом:" + salary + " руб.");
-            }
+        //Создайте абстрактный класс Shape с абстрактным методом draw().
+         abstract class Shape {
+            public abstract void draw();
         }
-        //Создайте подклассы Manager и Developer, которые наследуют от Employee. Добавьте метод work(), который выводит сообщение о работе сотрудника.
 
-        class Manager extends Employee {
-            public Manager(String name, int salary) {
-                super(name, salary);
-            }
-
+        // Создайте подклассы Circle и Rectangle, которые реализуют метод draw().
+         class Circle extends Shape {
             @Override
-            public void work() {
-                System.out.println(name + " руководит командой аналитиков и часто пьет кофе и получает зарплату:" + salary + " руб.");
+            public void draw() {
+                System.out.println("Нарисовать круг");
             }
         }
-        class Developer extends Employee {
-            public Developer(String name, int salary) {
-                super(name, salary);
-            }
-
+         class Rectangle extends Shape {
             @Override
-            public void work() {
-                System.out.println(name + " очень редко общается с командой аналитиков и поэтому часто ошибается при написании кода, но получает зарплату в размере:" + salary + " руб.");
+            public void draw() {
+                System.out.println("Нарисовать прямоугольник");
             }
         }
-        {
-                Employee employee = new Employee("Елена", 94700);
-                Manager manager = new Manager("Андрей", 135687);
-                Developer developer = new Developer("Артур", 142523);
-
-                employee.work();
-                manager.work();
-                developer.work();
-            }
-        }
+        // В методе draw() выведите сообщение о том, что фигура рисуется.
+        Shape Circle1 = new Circle();
+        Shape Rectangle1 = new Rectangle();
+        Circle1.draw();
+        Rectangle1.draw();
     }
+}
+
 
